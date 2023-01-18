3d63b763b23c7a51c828ffd9a19f792ca776ddda
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-18 10:33:54 -0300

    genre controller

--- api/src/controllers/genreControllers.js
+++ api/src/controllers/genreControllers.js
@@ -1,11 +1,10 @@
 const { Genre } = require('../db')
 const axios = require('axios')
-const { API_KEY } = process.env()
+const { API_KEY } = process.env
 
 const getApiGenres = async () => {
     const genresApi = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)
-
-    genresApi.forEach(async (g) => {
+    genresApi.data.results.forEach(async (g) => {
         await Genre.findOrCreate({
             where: {
                 name: g.name
