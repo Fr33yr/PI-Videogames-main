7cb62a42691f792bb779f3364ef7f4c3f3838378
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-19 10:57:52 -0300

    barrel update

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,5 @@
+export { default as Card } from './card/Card';
+export { default as Filters } from './filters/Filters';
 export { default as Layout } from './layout/Layout';
 export { default as Footer } from './layout/footer/Footer';
 export { default as Nav } from './layout/nav/Nav';
