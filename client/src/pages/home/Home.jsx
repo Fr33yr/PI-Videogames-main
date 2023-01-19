4c6bbe3f4a2bf7ac7af19d0c7788874814b8f930
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-19 11:51:47 -0300

    home links

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
+import { Link } from 'react-router-dom'
 import styles from './home.module.css'
 
 export default function Home() {
   return (
     <>
-
+      <Link to={'/games'}>Videogames</Link>
     </>
   )
 }
