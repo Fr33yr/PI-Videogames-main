1fe2ad1c4b6589d59ac9ad65777909cab3b754ba
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-19 11:51:36 -0300

    videogames components

--- client/src/pages/videogames/Videogames.jsx
+++ client/src/pages/videogames/Videogames.jsx
@@ -1,9 +1,17 @@
+import { Filters, Pagination, Card } from '../../components/index'
 import styles from './videogames.module.css'
 
 export default function Videogames() {
   return (
     <>
-    
+      <div className={styles.videogames}>
+        <Filters />
+        <div className={styles.cards}>
+          <Pagination />
+          <Card />
+          <Pagination />
+        </div>
+      </div>
     </>
   )
 }
