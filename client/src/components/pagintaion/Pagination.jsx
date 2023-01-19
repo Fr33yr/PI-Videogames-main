9732eb58c10b6a9c12e639aa763eda03761241fa
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-19 10:57:34 -0300

    pagination component

---
+++ client/src/components/pagintaion/Pagination.jsx
@@ -0,0 +1,13 @@
+import styles from './pagination.module.css'
+
+function Pagination() {
+    return (
+        <>
+            <div className={styles.pagination}>
+                
+            </div>
+        </>
+    )
+}
+
+export default Pagination
