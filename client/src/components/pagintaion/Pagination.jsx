bf54a69301f871685c2fee9c2c3943345b7f054f
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-21 18:05:32 -0300

    pagination component

--- client/src/components/pagintaion/Pagination.jsx
+++ client/src/components/pagintaion/Pagination.jsx
@@ -1,10 +1,23 @@
 import styles from './pagination.module.css'
 
-function Pagination() {
+function Pagination({ currentPage, setCurrentPage, nPages }) {
+    // === Local state ===
+    let prevPage = currentPage - 1
+    let nextPage = currentPage + 1
+
     return (
         <>
             <div className={styles.pagination}>
-                
+                {/* === Page buttons === */}
+                <button onClick={() => setCurrentPage(0)} disabled={nPages === 0 || currentPage}>first</button>
+                <button onClick={() => setCurrentPage(prevPage)} disabled={currentPage === 0 || nPages === 0}>{"<="}</button>
+                {/* === Page numbers === */}
+                <p>{prevPage >= 0 ? prevPage : ""}</p>
+                <p className={styles.currentPage}>{currentIndex}</p>
+                <p>{nextPage > nPages || nPages === 0 ? "" : nextPage}</p>
+                {/* === Page buttons === */}
+                <button onClick={() => setCurrentPage(nextPage)} disabled={currentPage === nPages || nPages === 0}>{"=>"}</button>
+                <button onClick={() => setCurrentPage(nPages)} disabled={nPages === 0 || currentIndex === nPages}>last</button>
             </div>
         </>
     )
