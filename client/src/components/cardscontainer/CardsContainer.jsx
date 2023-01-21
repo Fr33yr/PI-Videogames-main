a253710176439cc33b03fc3e9f18acacf4e0ed33
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-21 19:45:03 -0300

    props

--- client/src/components/cardscontainer/CardsContainer.jsx
+++ client/src/components/cardscontainer/CardsContainer.jsx
@@ -1,15 +1,12 @@
 import { Card } from '../index'
 
-export default function CardsContainer({ games }) {
+export default function CardsContainer(props) {
+    let {games} = props
 
     return (
         <>
             <div className="cardscontainer">
-                {games && games.map((g) => (
-                    <>
-                        <Card {...g} key={g.id}/>
-                    </>
-                ))}
+                {games && games.map((g) => (<Card {...g} key={g.id}/>))}
             </div>
         </>
     )
