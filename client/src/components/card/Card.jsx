2496d4c48e984caa885d9a705c2c71adad013896
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-21 19:44:48 -0300

    cards map needs revision

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -2,13 +2,14 @@ import { DetailBtn } from '../index'
 import styles from './card.module.css'
 
 export default function Card(props) {
-    const { name, genres, image, id } = props
+    let {genres, id, image, name, platforms, rating, released} = props
+
     return (
         <>
             <div className={styles.card}>
-                <img src={image ? image : "image not found"} alt={name} />
+                
                 <h3>{name}</h3>
-                {genres && genres.map((g, index) => (<h4 key={`${index}-${name}`}>{g}</h4>))}
+                
                 <DetailBtn id={id} />
             </div>
         </>
