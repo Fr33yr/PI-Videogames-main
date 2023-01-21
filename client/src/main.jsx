f4f0ec29f0a05d45a84df1856be2a3fff7d87e51
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-21 19:44:28 -0300

    ...

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,11 +7,9 @@ import './globals.css'
 import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
 )
