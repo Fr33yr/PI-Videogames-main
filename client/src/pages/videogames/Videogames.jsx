80ff58ed6540fffb22a0e13b91908fd929ccd640
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-21 18:06:17 -0300

    pagination state

--- client/src/pages/videogames/Videogames.jsx
+++ client/src/pages/videogames/Videogames.jsx
@@ -1,15 +1,24 @@
-import { Filters, Pagination, Card } from '../../components/index'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { CardsContainer, Filters, Pagination } from '../../components/index'
+import { usePaginate } from '../../hooks/usePaginate'
 import styles from './videogames.module.css'
 
 export default function Videogames() {
+  // === Local state ===
+  const [currentPage, setCurrentPage] = useState(0)
+
+  const games = useSelector(state => state.gamesCopy)
+  let { pages } = usePaginate(games, 15, 15)
+
   return (
     <>
       <div className={styles.videogames}>
         <Filters />
         <div className={styles.cards}>
-          <Pagination />
-          <Card />
-          <Pagination />
+          <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} />
+          <CardsContainer games={pages[currentPage]} />
+          <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} />
         </div>
       </div>
     </>
