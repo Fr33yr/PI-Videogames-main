1e61a463ce78ca43d7749e23127d74043d0a99e2
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-23 17:01:04 -0300

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
 import { Home, Detail, AddGame, Videogames } from '../src/pages/index'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { Layout } from '../src/components/index'
 
 function App() {
 
   return (
-    <Layout>
-      <main>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/games' element={<Videogames />} />
-          <Route path='/game/:id' element={<Detail />} />
-          <Route path='/addgame' element={<AddGame />} />
-        </Routes>
-      </main>
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <main>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/games' element={<Videogames />} />
+            <Route exact path='/game/:id' element={<Detail />} />
+            <Route exact path='/addgame' element={<AddGame />} />
+          </Routes>
+        </main>
+      </Layout>
+    </BrowserRouter>
   )
 }
 
