ab9b988191d21ea3bb65b3d08a97df9b0364ea99
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-23 17:33:34 -0300

    form validation hook

--- client/src/hooks/useValidate.js
+++ client/src/hooks/useValidate.js
@@ -1,5 +1,13 @@
 import { useState, useEffect } from "react";
 
-export function useValidate(){
-    
+export function useValidate(formInput){
+    const errorInitialState = {
+        textError: ''
+
+    }
+    const [error, setError] = useState({})
+
+    useEffect(()=>{
+
+    },[formInput])
 }
