a9b67d9ea304e33038ea4f921c0349d970bced90
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-23 12:20:01 -0300

    nav component links

--- client/src/components/layout/nav/Nav.jsx
+++ client/src/components/layout/nav/Nav.jsx
@@ -1,10 +1,16 @@
+import {Link} from 'react-router-dom'
+
 import styles from './nav.module.css'
 
 function Nav() {
   return (
     <>
         <nav className={styles.navbar}>
-            Nav
+            <ul>
+              <li>
+                <Link to={'/addgame'}>Add new game</Link>
+              </li>
+            </ul>
         </nav>
     </>
   )
