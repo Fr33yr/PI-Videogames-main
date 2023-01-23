e77b726f6307ad47f03a882b913435b6ce54fe98
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-23 12:20:17 -0300

    form import

--- client/src/pages/addgame/AddGame.jsx
+++ client/src/pages/addgame/AddGame.jsx
@@ -1,9 +1,10 @@
 import styles from './addgame.module.css'
+import {Form} from '../../components/index'
 
 export default function AddGame() {
   return (
     <>
-    
+      <Form />
     </>
   )
 }
