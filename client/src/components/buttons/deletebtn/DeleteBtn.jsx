aabbcb79a8081559b59408de38a7bf54801c4340
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-26 17:51:17 -0300

    edit and delete btn

---
+++ client/src/components/buttons/deletebtn/DeleteBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Deletebtn() {
+    return (
+        <div>Deletebtn</div>
+    )
+}
+
+export default Deletebtn
---
+++ client/src/components/buttons/editbtn/EditBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EditBtn() {
+    return (
+        <div>EditBtn</div>
+    )
+}
+
+export default EditBtn
