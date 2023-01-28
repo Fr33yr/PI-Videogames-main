d297d455fefd98a2a4f8de56b80049256882a7c6
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-28 20:01:18 -0300

    prevent duplicated options

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import styles from './form.module.css'
@@ -26,6 +26,7 @@ function Form() {
     let errors = validate(formValues)
 
     const dispatch = useDispatch()
+    
 
     // === Handlers === 
     const handleChange = (e) => {
@@ -36,6 +37,7 @@ function Form() {
 
     const handleAddGenre = (e) => {
         const value = e.target.value
+        !formValues.genres.includes(value) &&
         setFormValues({
             ...formValues,
             genres: [...formValues.genres, value]
@@ -44,6 +46,7 @@ function Form() {
 
     const handleAddPlatform = (e) => {
         const value = e.target.value
+        !formValues.platforms.includes(value) &&
         setFormValues({
             ...formValues,
             platforms: [...formValues.platforms, value]
@@ -52,13 +55,10 @@ function Form() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(Object.keys(errors).length);
         if (Object.keys(errors).length === 0) {
             dispatch(resetErrors())
-            //dispatch(createGame({ ...formValues }))
+            dispatch(createGame({ ...formValues }))
             setFormValues({ ...formValuesInitialState })
-        }else{
-            console.log(errors);
         }
     }
 
@@ -67,36 +67,46 @@ function Form() {
             <form action="" onSubmit={handleSubmit} className={styles.creationform}>
                 {/* === Name input === */}
                 <label htmlFor="">Name</label>
+                {errors.name && <h3>{errors.name}</h3>}
                 <input type="text" name="name" onChange={handleChange}
-                    autoComplete='off' value={formValues.name}/>
+                    autoComplete='off' value={formValues.name} />
+
                 {/* === Description input */}
                 <label htmlFor="">Description</label>
+                {errors.description && <h3>{errors.description}</h3>}
                 <textarea name="description" cols="30" rows="10"
                     onChange={handleChange} autoComplete='off'
                     value={formValues.description}></textarea>
+
                 {/* === Release date ===*/}
                 <label htmlFor="">Release date</label>
-                <input type="date" name="releaseDate" onChange={handleChange} 
-                value={formValues.releaseDate}/>
+                {errors.releaseDate}
+                <input type="date" name="releaseDate" onChange={handleChange}
+                    value={formValues.releaseDate} />
                 {/* === Image === */}
                 <label htmlFor="">Image url</label>
-                <input type="text" name='image' onChange={handleChange} 
-                value={formValues.image}/>
+                <input type="text" name='image' onChange={handleChange}
+                    value={formValues.image} />
+
                 {/* === Rating === */}
                 <label htmlFor="">Rating</label>
                 <input type="number" name="rating" max={5} min={0}
-                    onChange={handleChange} value={formValues.rating}/>
+                    onChange={handleChange} value={formValues.rating} />
+
                 {/* === Genres === */}
                 <label htmlFor="">Genre</label>
-                <select onChange={handleAddGenre} value={formValues.genres.length === 0? '--- choose genres ---' : formValues.genres.length}>
+                <select onChange={handleAddGenre} value={formValues.genres.length === 0 ? '--- choose genres ---' :
+                    formValues.genres.length}>
                     <option value="--- choose genres ---" disabled={true}>--- choose genres ---</option>
                     {genresOptions && genresOptions.map((g) => (
-                        <option value={g.name} key={g.id}>{g.name}</option>
+                        <option value={g.name} key={g.id} >{g.name}</option>
                     ))}
                 </select>
+
                 {/* === Platforms === */}
                 <label htmlFor="">Platform</label>
-                <select onChange={handleAddPlatform} value={formValues.platforms.length === 0? '--- choose platforms ---' : formValues.platforms.length} >
+                <select onChange={handleAddPlatform} value={formValues.platforms.length === 0 ? '--- choose platforms ---' :
+                    formValues.platforms.length} >
                     <option value="--- choose platforms ---" disabled={true}>--- choose platforms ---</option>
                     {platformsOptions && platformsOptions.map((p) => (
                         <option value={p.name} key={p.id}>{p.name}</option>
