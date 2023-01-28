e24e4c089c1fa4e27621c410f76942e331c785b3
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-28 18:42:19 -0300

    form validation and form reset

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -4,7 +4,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import styles from './form.module.css'
 import { resetErrors } from '../../redux/actions/errorActions'
 import { createGame } from '../../redux/actions/gamesActions'
-import { useGenres, usePaltforms, useValidate } from '../../hooks/index'
+import { useGenres, usePaltforms } from '../../hooks/index'
+import { validate } from '../../utils/validate'
 
 function Form() {
     // === Local state ===
@@ -22,9 +23,9 @@ function Form() {
 
     const { genresOptions } = useGenres()
     const { platformsOptions } = usePaltforms()
+    let errors = validate(formValues)
 
     const dispatch = useDispatch()
-
 
     // === Handlers === 
     const handleChange = (e) => {
@@ -51,9 +52,14 @@ function Form() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        //dispatch(resetErrors())
-        //dispatch(createGame({ ...formValues, genres, platforms }))
-        setFormValues({ ...formValuesInitialState })
+        console.log(Object.keys(errors).length);
+        if (Object.keys(errors).length === 0) {
+            dispatch(resetErrors())
+            //dispatch(createGame({ ...formValues }))
+            setFormValues({ ...formValuesInitialState })
+        }else{
+            console.log(errors);
+        }
     }
 
     return (
@@ -62,24 +68,27 @@ function Form() {
                 {/* === Name input === */}
                 <label htmlFor="">Name</label>
                 <input type="text" name="name" onChange={handleChange}
-                    autoComplete='off' />
+                    autoComplete='off' value={formValues.name}/>
                 {/* === Description input */}
                 <label htmlFor="">Description</label>
-                <textarea name="description" id="" cols="30" rows="10"
-                    onChange={handleChange} autoComplete='off'></textarea>
+                <textarea name="description" cols="30" rows="10"
+                    onChange={handleChange} autoComplete='off'
+                    value={formValues.description}></textarea>
                 {/* === Release date ===*/}
                 <label htmlFor="">Release date</label>
-                <input type="date" name="releaseDate" onChange={handleChange} />
+                <input type="date" name="releaseDate" onChange={handleChange} 
+                value={formValues.releaseDate}/>
                 {/* === Image === */}
                 <label htmlFor="">Image url</label>
-                <input type="text" name='image' onChange={handleChange} />
+                <input type="text" name='image' onChange={handleChange} 
+                value={formValues.image}/>
                 {/* === Rating === */}
                 <label htmlFor="">Rating</label>
                 <input type="number" name="rating" max={5} min={0}
-                    onChange={handleChange} />
+                    onChange={handleChange} value={formValues.rating}/>
                 {/* === Genres === */}
                 <label htmlFor="">Genre</label>
-                <select onChange={handleAddGenre} defaultValue={'--- choose genres ---'}>
+                <select onChange={handleAddGenre} value={formValues.genres.length === 0? '--- choose genres ---' : formValues.genres.length}>
                     <option value="--- choose genres ---" disabled={true}>--- choose genres ---</option>
                     {genresOptions && genresOptions.map((g) => (
                         <option value={g.name} key={g.id}>{g.name}</option>
@@ -87,7 +96,7 @@ function Form() {
                 </select>
                 {/* === Platforms === */}
                 <label htmlFor="">Platform</label>
-                <select onChange={handleAddPlatform} defaultValue={'--- choose platforms ---'}>
+                <select onChange={handleAddPlatform} value={formValues.platforms.length === 0? '--- choose platforms ---' : formValues.platforms.length} >
                     <option value="--- choose platforms ---" disabled={true}>--- choose platforms ---</option>
                     {platformsOptions && platformsOptions.map((p) => (
                         <option value={p.name} key={p.id}>{p.name}</option>
