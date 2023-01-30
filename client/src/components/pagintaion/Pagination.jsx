7c9baee86a0bbb5b4e4e33c1b10cfc2d2ce2569e
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-30 16:02:22 -0300

    last page detail

--- client/src/components/pagintaion/Pagination.jsx
+++ client/src/components/pagintaion/Pagination.jsx
@@ -15,6 +15,7 @@ function Pagination({ currentPage, setCurrentPage, nPages }) {
                 <p>{prevPage >= 0 ? prevPage : ""}</p>
                 <p className={styles.currentPage}>{currentPage}</p>
                 <p>{nextPage > nPages || nPages === 0 ? "" : nextPage}</p>
+                <p>{nPages - 1 === currentPage || nPages === currentPage? "" : `...${nPages}`}</p>
                 {/* === Page buttons === */}
                 <button onClick={() => setCurrentPage(nextPage)} disabled={currentPage === nPages || nPages === 0}>{"=>"}</button>
                 <button onClick={() => setCurrentPage(nPages)} disabled={nPages === 0 || currentPage === nPages}>last</button>
