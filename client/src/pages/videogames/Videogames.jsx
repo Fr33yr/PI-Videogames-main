75c1d3d3ec34454878fa78345426d8d1c66b420d
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-31 10:24:59 -0300

    loader added

--- client/src/pages/videogames/Videogames.jsx
+++ client/src/pages/videogames/Videogames.jsx
@@ -1,32 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
-import { CardsContainer, Filters, Pagination } from '../../components/index'
+import { CardsContainer, Filters, Loader, Pagination } from '../../components/index'
 import { Paginate } from '../../utils/paginate'
 import styles from './videogames.module.css'
 
 export default function Videogames() {
   // === Local state ===
   const [currentPage, setCurrentPage] = useState(0)
+  const [loading, setLoading] = useState(false)
 
   const games = useSelector(state => state.gamesCopy)
   let pages = Paginate(games, 15, 15)
 
+  useEffect(() => {
+    !games || games.length === 0 ? setLoading(true) : setLoading(false)
+  }, [games])
+
   return (
     <>
-      <div className={styles.videogames}>
-        <Filters />
-        <div className={styles.cards}>
-          
-          <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage}
-            nPages={Array.isArray(pages[0]) ? pages.length - 1 : 0} />
+      {loading ? <Loader /> :
+        <div className={styles.videogames}>
+          <Filters />
+          <div className={styles.cards}>
+            <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage}
+              nPages={Array.isArray(pages[0]) ? pages.length - 1 : 0} />
 
-          {Array.isArray(pages[0]) ? <CardsContainer games={pages[currentPage]} /> : <CardsContainer games={pages} />}
+            {Array.isArray(pages[0]) ? <CardsContainer games={pages[currentPage]} /> : <CardsContainer games={pages} />}
 
-          <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage}
-            nPages={Array.isArray(pages[0]) ? pages.length - 1 : 0} />
+            <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage}
+              nPages={Array.isArray(pages[0]) ? pages.length - 1 : 0} />
+          </div>
         </div>
-      </div>
+      }
     </>
+
   )
 }
