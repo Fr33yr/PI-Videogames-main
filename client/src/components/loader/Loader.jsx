a1739c172413cf63e7c4d390b784fccebb6795b5
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-31 10:01:05 -0300

    spinner

---
+++ client/src/components/loader/Loader.jsx
@@ -0,0 +1,11 @@
+import styles from './loader.module.css'
+
+function Loader() {
+  return (
+    <>
+    <div class={styles.ldsring}><div></div><div></div><div></div><div></div></div>
+    </>
+  )
+}
+
+export default Loader
