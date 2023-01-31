91cb944074827c3f4b459f0f4e96cc6811b88ac6
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-01-31 10:01:11 -0300

    barrel update

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -15,4 +15,5 @@ export { default as Rating } from './form/inputs/Rating';
 export { default as Layout } from './layout/Layout';
 export { default as Footer } from './layout/footer/Footer';
 export { default as Nav } from './layout/nav/Nav';
+export { default as Loader } from './loader/Loader';
 export { default as Pagination } from './pagintaion/Pagination';
