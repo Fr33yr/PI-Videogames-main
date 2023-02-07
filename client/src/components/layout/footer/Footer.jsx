5ccd373e89d0ce1da873fa071a77a71e898d7984
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-02-07 07:24:17 -0300

    hide footer on landing page

--- client/src/components/layout/footer/Footer.jsx
+++ client/src/components/layout/footer/Footer.jsx
@@ -1,14 +1,21 @@
+import { useEffect } from 'react'
 import { useLocation, } from 'react-router-dom'
 import styles from './footer.module.css'
 
 function Footer() {
   let location = useLocation()
-  
+  useEffect(() => {
+
+  }, [])
   return (
     <>
-        <footer className={styles.footer}>
-          <p>Powered by</p><a href="https://rawg.io/apidocs" target="_blank" rel="noopener noreferrer">Video Games Database API</a>
-        </footer>
+      {location.pathname === '/' ? "" :
+        <>
+          <footer className={styles.footer}>
+            <p>Powered by</p><a href="https://rawg.io/apidocs" target="_blank" rel="noopener noreferrer">Video Games Database API</a>
+          </footer>
+        </>
+      }
     </>
   )
 }
